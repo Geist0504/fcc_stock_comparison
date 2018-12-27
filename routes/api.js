19b5dfd1753f70bd31d40b36c0e00ac1d1f67cc4
Glitch (mercury-stone) <none>
2018-12-27 23:06:26 +0000

    🍜😀 Checkpoint ./package.json:2025103/40 ./routes/api.js:2025103/597

--- routes/api.js
+++ routes/api.js
@@ -11,7 +11,9 @@
 var expect = require('chai').expect;
 var MongoClient = require('mongodb');
 let fetch = require('node-fetch')
-let yahooStockPrices = require("yahoo-stock-prices")
+const si = require('stock-info');
+const stocks = ['AMZN', 'NFLIX'];
+
 
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
@@ -21,11 +23,9 @@ module.exports = function (app) {
 
   app.route('/api/stock-prices')
     .get(async function (req, res){
-    //let test = await fetch('https://finance.google.com/finance/info?q=NASDAQ%3aGOOG')
-    yahooStockPrices.getCurrentPrice('AAPL', function(err, price){
-      console.log(price);
-    });
-    res.send('working')
+      let test = await si.getStocksInfo(stocks)
+      console.log(test[0].regularMarketPrice)
+      res.send('working')
     
     MongoClient.connect(CONNECTION_STRING, function(err, db) {
         let collection = db.collection(db_collection)
