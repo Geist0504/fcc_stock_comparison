dc6075a1bebbcb3c6a77c2300afc0c617dcd76b0
Glitch (mercury-stone) <none>
2018-12-27 21:18:22 +0000

    🌮🚞 Checkpoint ./tests/2_functional-tests.js:2025103/25 ./routes/api.js:2025103/21

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -22,7 +22,7 @@ suite('Functional Tests', function() {
         .get('/api/stock-prices')
         .query({stock: 'goog'})
         .end(function(err, res){
-          
+          console.log('test')
           //complete this one too
           
           done();
