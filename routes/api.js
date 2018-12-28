7480f52c5ec2dd594cb062a06e9b0101ba5b2fa2
Glitch (mercury-stone) <none>
2018-12-28 19:51:07 +0000

    💋🏐 Checkpoint ./routes/api.js:2025103/154

--- routes/api.js
+++ routes/api.js
@@ -24,16 +24,15 @@ module.exports = function (app) {
   app.route('/api/stock-prices')
     .get(async function (req, res){
       let stock_requests = req.query.stock
-      console.log(stock_requests, typeof(stock_requests))
-      typeof(stock_requests) == 'string' ? stock_requests = stock_requests.map(function(x){ return x.toUpperCase() })
+      typeof(stock_requests) == 'string' ? stock_requests = [stock_requests.toUpperCase()] : stock_requests = stock_requests.map(function(x){ return x.toUpperCase() })
       let like = req.query.like
       console.log(stock_requests)
-      let test = await si.getStocksInfo(stocks)
+      let test = await si.getStocksInfo(stock_requests)
       res.send('working')
     
     MongoClient.connect(CONNECTION_STRING, function(err, db) {
         let collection = db.collection(db_collection)
-        stocks.forEach((stock) =>{
+        stock_requests.forEach((stock) =>{
           let stockObj = test.find(obj => {
             return obj.symbol === stock
           })
