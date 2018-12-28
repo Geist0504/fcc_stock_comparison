fe70fb89b8f6a250ea4cd98fec133d71d9df0060
Glitch (mercury-stone) <none>
2018-12-28 20:24:09 +0000

    🚦🎄 Checkpoint ./routes/api.js:2025103/1027

--- routes/api.js
+++ routes/api.js
@@ -27,10 +27,9 @@ module.exports = function (app) {
     .get(async function (req, res){
       let stock_requests = req.query.stock
       typeof(stock_requests) == 'string' ? stock_requests = [stock_requests.toUpperCase()] : stock_requests = stock_requests.map(function(x){ return x.toUpperCase() })
-      let like = req.query.like
-      console.log(stock_requests)
+      let like = undefined
+      console.log(like !== undefined)
       let test = await si.getStocksInfo(stock_requests)
-      res.json(stock_data)
     
     MongoClient.connect(CONNECTION_STRING, function(err, db) {
         let collection = db.collection(db_collection)
@@ -38,13 +37,20 @@ module.exports = function (app) {
           let stockObj = test.find(obj => {
             return obj.symbol === stock
           })
-          
-          collection.findOneAndUpdate({name: stock}, {name:stock,price:stockObj.regularMarketPrice}, {upsert:true, returnOriginal:false}, (err, data) =>{
-          })
-          if(like){
+          console.log(stock_data)
+          if(like !== undefined){
             collection.findOneAndUpdate({name: stock}, {$inc:{likes:1}}, {returnOriginal:false}, (err, data) =>{
               console.log(data.value)
-            })}
+              console.log(stock_data)
+          })}
+          else {
+            collection.findOneAndUpdate({name: stock}, {name:stock,price:stockObj.regularMarketPrice}, {upsert:true, returnOriginal:false}, (err, data) =>{
+              stock_data.stockData.append(data.value)
+              console.log(data.value)
+              console.log(stock_data)
+              })
+            }
+          
         })
         res.json(stock_data)
       })
